c76011423f3628c9434527d00ce12fc4fdaa4931
Bruno Talanski <brn.talanski@gmail.com>
2021-06-17 20:49:36 -0300

    Add placeholder menu items

--- src/js/components/AppDrawer/index.js
+++ src/js/components/AppDrawer/index.js
@@ -4,17 +4,35 @@ import {
   SwipeableDrawer,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
+  ListItemIcon
 } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import TuneIcon from '@material-ui/icons/Tune';
 
 export const AppDrawer = (props) => {
+  const menuItems = [
+    {
+      label: 'Stream status',
+      target: '#',
+      icon: <TuneIcon></TuneIcon>
+    },
+    {
+      label: 'Output settings',
+      target: '#',
+      icon: <TuneIcon></TuneIcon>
+    }
+  ];
   return (
     <SwipeableDrawer anchor="left" {...props}>
       <Box width={250}>
         <List>
-          <ListItem>
-            <ListItemText primary={'Teste'}></ListItemText>
-          </ListItem>
+          {menuItems.map((item) => (
+            <ListItem key={item.label}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label}></ListItemText>
+            </ListItem>
+          ))}
         </List>
       </Box>
     </SwipeableDrawer>
