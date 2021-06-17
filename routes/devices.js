ce34b7b0f58b03857e0f046fe30f951f7a4e2186
Bruno Talanski <brn.talanski@gmail.com>
2021-06-17 19:07:29 -0300

    Update material ui

--- routes/devices.js
+++ routes/devices.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var v4l2camera = require("../node_modules/v4l2camera");
+// var v4l2camera = require("../node_modules/v4l2camera");
 
 
 router.get('/', function(req, res, next) {
-    var cam = new v4l2camera.Camera("/dev/video0");
-    const formats = cam.formats;
-    res.send(cam.formats.filter(format => format.formatName === "H264"));
+    // var cam = new v4l2camera.Camera("/dev/video0");
+    // const formats = cam.formats;
+    // res.send(cam.formats.filter(format => format.formatName === "H264"));
 });
 
 router.get('/controls', function(req, res, next) {
-    var cam = new v4l2camera.Camera("/dev/video0");
-    const controls = cam.controls || [];
-    res.send(controls);
+    // var cam = new v4l2camera.Camera("/dev/video0");
+    // const controls = cam.controls || [];
+    // res.send(controls);
 });
 
 module.exports = router;
--- src/js/components/App/index.js
+++ src/js/components/App/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Typography
+} from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
 
 export const App = () => {
-  return <Button color="primary">Hello World</Button>;
-}
+  return (
+    <React.Fragment>
+      <CssBaseline>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              My Stream Controller
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </CssBaseline>
+    </React.Fragment>
+  );
+};
